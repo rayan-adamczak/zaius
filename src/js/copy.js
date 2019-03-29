a5617748957e7233edac399c48d34d4afa51d07d
Rayan Adamczak <ag-hiro@hotmail.fr>
2019-03-29 17:27:26 +0100

    goodmodal

--- src/js/copy.js
+++ src/js/copy.js
@@ -1,4 +1,3 @@
-
 // Creation of the function copy
 function copyToClipboard(text){
   var dummy = document.createElement("input");
@@ -11,6 +10,19 @@ function copyToClipboard(text){
 
 // Using of the function
 document.getElementById("mail").addEventListener("click", copyToClipboard("oscar@cyberbea.com"));
-document.getElementById("mail").addEventListener("click", function(){
-  alert('copied!');
+
+
+// modal
+document.getElementById('mail').addEventListener('click', function() {
+  document.getElementById('overlay').classList.add('is-visible');
+  document.getElementById('modal').classList.add('is-visible');
+});
+
+document.getElementById('close-btn').addEventListener('click', function() {
+  document.getElementById('overlay').classList.remove('is-visible');
+  document.getElementById('modal').classList.remove('is-visible');
+});
+document.getElementById('overlay').addEventListener('click', function() {
+  document.getElementById('overlay').classList.remove('is-visible');
+  document.getElementById('modal').classList.remove('is-visible');
 });
