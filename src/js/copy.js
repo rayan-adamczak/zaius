d84a1eb58d8dae54d95d6050a83e72a4a3bf0510
Rayan Adamczak <ag-hiro@hotmail.fr>
2019-03-30 12:46:26 +0100

    scrolleffect

--- src/js/copy.js
+++ src/js/copy.js
@@ -1,3 +1,42 @@
+jQuery(function($) {
+  
+  // Function which adds the 'animated' class to any '.animatable' in view
+  var doAnimations = function() {
+    
+    // Calc current offset and get all animatables
+    var offset = $(window).scrollTop() + $(window).height(),
+        $animatables = $('.animatable');
+    
+    // Unbind scroll handler if we have no animatables
+    if ($animatables.length == 0) {
+      $(window).off('scroll', doAnimations);
+    }
+    
+    // Check all animatables and animate them if necessary
+		$animatables.each(function(i) {
+       var $animatable = $(this);
+			if (($animatable.offset().top + $animatable.height() - 200) < offset) {
+        $animatable.removeClass('animatable').addClass('animated');
+			}
+    });
+
+	};
+  
+  // Hook doAnimations on scroll, and trigger a scroll
+	$(window).on('scroll', doAnimations);
+  $(window).trigger('scroll');
+
+});
+
+
+
+
+
+
+
+
+
+
 // Creation of the function copy
 function copyToClipboard(text){
   var dummy = document.createElement("input");
@@ -26,3 +65,11 @@ document.getElementById('overlay').addEventListener('click', function() {
   document.getElementById('overlay').classList.remove('is-visible');
   document.getElementById('modal').classList.remove('is-visible');
 });
+
+
+
+
+
+
+
+
