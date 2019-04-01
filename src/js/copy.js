d8a0abb90c3ca3447813d7e94ddf677a7c94b08b
Rayan Adamczak <ag-hiro@hotmail.fr>
2019-04-01 09:37:34 +0200

    newcopyclipboard

--- src/js/copy.js
+++ src/js/copy.js
@@ -1,3 +1,4 @@
+// Text appears fadeintop
 jQuery(function($) {
   
   // Function which adds the 'animated' class to any '.animatable' in view
@@ -56,20 +57,72 @@ jQuery(function($) {
 
 
 
-function copyToClipBoard(text) {
-  try {
-      var $temp = $("<input>");
-      $("body").append($temp);
-      $temp.val(text).select();
-      var retVal = document.execCommand("copy");
-      console.log('Copy to clipboard returns: ' + retVal);
-      $temp.remove();
-  } catch (err) {
-      console.log('Error while copying to clipboard: ' + err);
-  }
-}
+// function copyToClipBoard(text) {
+//   try {
+//       var $temp = $("<input>");
+//       $("body").append($temp);
+//       $temp.val(text).select();
+//       var retVal = document.execCommand("copy");
+//       console.log('Copy to clipboard returns: ' + retVal);
+//       $temp.remove();
+//   } catch (err) {
+//       console.log('Error while copying to clipboard: ' + err);
+//   }
+// }
 
-$("mail").on('click', copyToClipBoard('oscar@cyberbea.com'));
+// $("mail").on('click', copyToClipBoard('oscar@cyberbea.com'));
+
+
+  
+document.getElementById('mail').addEventListener( 'click', function( event ) { copytext("oscar") } );
+
+
+    function detectIE() {
+        var ua = window.navigator.userAgent;
+
+        var msie = ua.indexOf('MSIE ');
+        if (msie > 0) {
+            // IE 10 or older => return version number
+            return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+        }
+
+        var trident = ua.indexOf('Trident/');
+        if (trident > 0) {
+            // IE 11 => return version number
+            var rv = ua.indexOf('rv:');
+            return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+        }
+
+        var edge = ua.indexOf('Edge/');
+        if (edge > 0) {
+            // IE 12 => return version number
+            return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+        }
+
+        // other browser
+        return false;
+    }
+    function copytext(text) {
+        if (detectIE()) {
+            window.clipboardData.setData('Text', text);
+        }
+        var textField = document.createElement('textarea');
+        textField.innerText = text;
+        document.body.appendChild(textField);
+        textField.select();
+        document.execCommand('copy');
+        window.clipboardData.setData('Text', copytext);
+        textField.remove();
+    }
+
+function copytext(text) {
+        var textField = document.createElement('textarea');
+        textField.innerText = text;
+        document.body.appendChild(textField);
+        textField.select();
+        document.execCommand('copy');
+        $(textField).remove();
+    }
 
 
 
